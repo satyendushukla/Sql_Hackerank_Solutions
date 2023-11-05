83cac423cd70b39524168d2b3381fc58ad3d4ff5
satyendushukla <satyendu.21b1541157@abes.ac.in>
2023-11-05 19:14:05 +0530

    first

---
+++ basicSelect/Weather Observation Station 11.sql
@@ -0,0 +1,4 @@
+/*Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. Your result cannot contain duplicates.*/
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY NOT REGEXP '^[AEIOUaeiou]' OR CITY NOT REGEXP '[AEIOUaeiou]$';
