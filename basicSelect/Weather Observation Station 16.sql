e755652a3e5f08c2ab45a6a4cdd9606534efbe11
satyendushukla <satyendu.21b1541157@abes.ac.in>
2024-05-30 23:58:56 +0530

    t

---
+++ basicSelect/Weather Observation Station 16.sql
@@ -0,0 +1,7 @@
+/*Query the smallest Northern Latitude (LAT_N) from STATION that is greater than 38.780 . Round your answer to  decimal places.*/
+
+SELECT
+    ROUND(LAT_N, 4)
+FROM Station
+WHERE LAT_N > 38.7780
+ORDER BY LAT_N limit 1;
